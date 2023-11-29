29b76a443a0a665fc758a9e52abb191a25d973d2
augustboyy <august06180@gmail.com>
2023-11-30 01:19:39 +0900

    첫 화면과 메인 메뉴선택화면

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,11 @@
 // scripts/script.js
 
-document.getElementById('myButton').addEventListener('click', function() {
-    // Option 1을 클릭했을 때 새로운 페이지로 이동
-    window.location.href = 'page1.html';
+document.getElementById('recommandMenu').addEventListener('click', function() {
+    // 추천메뉴 을 클릭했을 때 recommandMenu 페이지로 이동
+    window.location.href = 'recommandMenu.html';
 });
 
-document.getElementById('myButton').addEventListener('click', function() {
-    // Option 1을 클릭했을 때 새로운 페이지로 이동
-    window.location.href = 'page1.html';
+document.getElementById('chooseMenu').addEventListener('click', function() {
+    // 메뉴 고르기을 클릭했을 때 choosedMenu 페이지로 이동
+    window.location.href = 'chooseMenu.html';
 });
-
