e2ae19ea7eeade2df22af667052418ed568f09b2
augustboyy <august06180@gmail.com>
2023-11-30 01:10:46 +0900

    첫 화면과 메인 선택화면

---
+++ scripts/hello.js
@@ -0,0 +1,4 @@
+document.getElementById('myCard').addEventListener('click', function() {
+    // 클릭하면 다른 페이지로 이동
+    window.location.href = 'index.html';
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // scripts/script.js
 
-document.getElementById('option1').addEventListener('click', function() {
+document.getElementById('myButton').addEventListener('click', function() {
     // Option 1을 클릭했을 때 새로운 페이지로 이동
     window.location.href = 'page1.html';
 });
@@ -9,3 +9,4 @@ document.getElementById('myButton').addEventListener('click', function() {
     // Option 1을 클릭했을 때 새로운 페이지로 이동
     window.location.href = 'page1.html';
 });
+
