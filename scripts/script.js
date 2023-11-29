c1f053117078c68197a361b68c5f33dbd6244da0
augustboyy <august06180@gmail.com>
2023-11-29 21:41:30 +0900

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// scripts/script.js
+
+document.getElementById('option1').addEventListener('click', function() {
+    // Option 1을 클릭했을 때 새로운 페이지로 이동
+    window.location.href = 'page1.html';
+});
+
+document.getElementById('myButton').addEventListener('click', function() {
+    // Option 1을 클릭했을 때 새로운 페이지로 이동
+    window.location.href = 'page1.html';
+});
