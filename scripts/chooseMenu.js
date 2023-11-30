2d6120909f4b7133a1b8bc0e66722633a972aaca
augustboyy <august06180@gmail.com>
2023-12-01 01:42:13 +0900

    pay 창 분할생성

--- scripts/chooseMenu.js
+++ scripts/chooseMenu.js
@@ -1,19 +1,19 @@
 document.getElementById('menu').addEventListener('click', function() {
     // 메뉴1을 클릭했을 때 pay 페이지로 이동
-    window.location.href = 'pay.html';
+    window.location.href = 'payOne.html';
 });
 
 document.getElementById('menuTwo').addEventListener('click', function() {
     // 메뉴2 클릭했을 때 pay 페이지로 이동
-    window.location.href = 'pay.html';
+    window.location.href = 'payTwo.html';
 });
 
 document.getElementById('menuThr').addEventListener('click', function() {
     // 메뉴3 클릭했을 때 pay 페이지로 이동
-    window.location.href = 'pay.html';
+    window.location.href = 'payThr.html';
 });
 
 document.getElementById('menuFor').addEventListener('click', function() {
     // 메뉴4 클릭했을 때 pay 페이지로 이동
-    window.location.href = 'pay.html';
+    window.location.href = 'payFor.html';
 });
---
+++ scripts/pay.js
@@ -0,0 +1,4 @@
+document.getElementById('container').addEventListener('click', function() {
+    // 클릭하면  페이지로 이동
+    window.location.href = 'index.html';
+});
