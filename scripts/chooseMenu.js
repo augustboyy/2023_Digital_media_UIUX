dd403a115ce1a9281d4ef0efef47f34088833a35
augustboyy <august06180@gmail.com>
2023-11-30 23:15:13 +0900

    메뉴 고르기 완성, pay창 생성

--- scripts/chooseMenu.js
+++ scripts/chooseMenu.js
@@ -1,4 +1,19 @@
 document.getElementById('menu').addEventListener('click', function() {
-    // 메뉴1dmf 클릭했을 때 choosedMenu 페이지로 이동
-    window.location.href = 'chooseMenu.html';
+    // 메뉴1을 클릭했을 때 pay 페이지로 이동
+    window.location.href = 'pay.html';
+});
+
+document.getElementById('menuTwo').addEventListener('click', function() {
+    // 메뉴2 클릭했을 때 pay 페이지로 이동
+    window.location.href = 'pay.html';
+});
+
+document.getElementById('menuThr').addEventListener('click', function() {
+    // 메뉴3 클릭했을 때 pay 페이지로 이동
+    window.location.href = 'pay.html';
+});
+
+document.getElementById('menuFor').addEventListener('click', function() {
+    // 메뉴4 클릭했을 때 pay 페이지로 이동
+    window.location.href = 'pay.html';
 });
